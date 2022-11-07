9f25d3216491048b98fc6a5cb5cb86220c91bc6d
mtkamiloglu <70536420+mtkamiloglu@users.noreply.github.com>
2022-11-07 22:15:31 +0300

    Create task.cpp

---
+++ task.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+
+#define G 9.80665
+
+int main()
+{
+float reach;
+float time;
+
+std::cin >> time;
+rach = 0.5*time*time*G;
+std::cout << reach << std::endl;
+
+return 0;
+}
